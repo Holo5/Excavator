b6e72d5806a9afb752218c8085436dd2b719e38e
Pi-Bouf <pierre.b@onatera.com>
2020-06-19 16:55:30 +0200

    Adding Listr for async tasks

--- Server/src/App/TaskRunner/FigureExtractorTaskRunner.ts
+++ Server/src/App/TaskRunner/FigureExtractorTaskRunner.ts
@@ -2,6 +2,9 @@ import {inject, singleton} from "tsyringe";
 import {FigureMapExtractor} from "../../Extractor/FigureMapExtractor";
 import {Lib} from "../../Domain/FigureMap/Lib";
 import {ExtractionState} from "../../Domain/FigureMap/Enum/ExtractionState";
+import * as Listr from "listr";
+
+// @ts-ignore
 
 @singleton()
 export class FigureExtractorTaskRunner {
@@ -16,11 +19,68 @@ export class FigureExtractorTaskRunner {
   startExtraction() {
     this._libsToExtract = [];
     this.trimWaitingLib();
+
+    let taks = new Listr([
+        this.getRandomTask()
+      ]
+    );
+
+    taks.run().then(() => {
+      console.log("HEY");
+
+    });
+  }
+
+  getRandomTask() {
+    return {
+      title: "Test :D " + (Math.random() * 15000).toString(),
+      task: () => new Listr([
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+        {
+          title: "Test :D " + (Math.random() * 15000).toString(),
+          task: this.getDelayedTask
+        },
+
+      ])
+    }
+  }
+
+  getDelayedTask() {
+    return new Promise(resolve => {
+      setTimeout(resolve, Math.random() * 1000)
+    });
   }
 
   private trimWaitingLib() {
     this._figureMapExtractor.libs.forEach(lib => {
-      if(lib.extractionState === ExtractionState.WAITING) {
+      if (lib.extractionState === ExtractionState.WAITING) {
         this._libsToExtract.push(lib);
       }
     });
