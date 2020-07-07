e0e44e5bc72b2b3d3ef8def9dfbfb2d01f360460
Pi-Bouf <pierre.b@onatera.com>
2020-07-07 12:46:25 +0200

    Fix algorithm for animations

--- Server/src/App/Retriever/AvatarAnimationRetriever.ts
+++ Server/src/App/Retriever/AvatarAnimationRetriever.ts
@@ -32,22 +32,25 @@ export class AvatarAnimationRetriever {
             return this.getTypes(key, regExp);
         });
 
-        const layersId = this.findAllLayerId(types);
-        const gestures = this.findAllGestures(types);
+        const bodyParts = this.findAllBodyParts(types);
+        bodyParts.forEach(bodyPart => {
+            const gestures = this.findAllGestures(types, bodyPart);
+            const layersId = this.findAllLayerId(types, bodyPart);
 
-        layersId.forEach((layerId) => {
-            gestures.forEach((gesture) => {
-                const gestureData = AvatarDirectionAngle.GESTURE_DATA[gesture];
-                if (gestureData === undefined) {
-                    Logger.error(`Gesture ${gesture} not found for ${id} !`);
-                }
-
-                gestureData.direction.forEach((dir) => {
-                    const frames = [];
-                    for (let frame = 0; frame < gestureData.framesCount; frame++) {
-                        frames.push(this._habboAvatarAsset.find(id, { direction: dir, layerId, frame: frame.toString(), gesture, partType: types[0].partType }, spritesheet));
+            layersId.forEach((layerId) => {
+                gestures.forEach((gesture) => {
+                    const gestureData = AvatarDirectionAngle.GESTURE_DATA[gesture];
+                    if (gestureData === undefined) {
+                        Logger.error(`Gesture ${gesture} not found for ${id} !`);
                     }
-                    animations[`${types[0].partType}_${layerId}_${gesture}_${dir}`] = frames;
+
+                    gestureData.direction.forEach((dir) => {
+                        const frames = [];
+                        for (let frame = 0; frame < gestureData.framesCount; frame++) {
+                            frames.push(this._habboAvatarAsset.find(id, { direction: dir, layerId, frame: frame.toString(), gesture, partType: bodyPart }, spritesheet));
+                        }
+                        animations[`${bodyPart}_${layerId}_${gesture}_${dir}`] = frames;
+                    });
                 });
             });
         });
@@ -56,12 +59,16 @@ export class AvatarAnimationRetriever {
         this._fsRepository.writeSpriteSheet(id, JSON.stringify(spritesheet));
     }
 
-    private findAllGestures(types: IType[]): string[] {
-        return Array.from(new Set(types.map((type) => type.gesture)));
+    private findAllGestures(types: IType[], bodyPart: string): string[] {
+        return Array.from(new Set(types.filter(type => type.partType === bodyPart).map((type) => type.gesture)));
     }
 
-    private findAllLayerId(types: IType[]): string[] {
-        return Array.from(new Set(types.map((type) => type.layerId)));
+    private findAllLayerId(types: IType[], bodyPart: string): string[] {
+        return Array.from(new Set(types.filter(type => type.partType === bodyPart).map((type) => type.layerId)));
+    }
+
+    private findAllBodyParts(types: IType[]): string[] {
+        return Array.from(new Set(types.map((type) => type.partType)));
     }
 
     private getTypes(key: string, regExp: RegExp): IType {
