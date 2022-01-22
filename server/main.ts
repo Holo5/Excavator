edcc1909356f427675486c31bebfc8936dd9810d
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-01-23 00:06:53 +0100

    Update main

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { container } from 'tsyringe';
-import { Holo5Excavator } from './src/App/Holo5Excavator';
+import { Holo5Excavator } from './src/app/Holo5Excavator';
 
 const app = container.resolve(Holo5Excavator);
 app.init();
