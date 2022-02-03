18752197195a5082fabe66debee6cf660c0b17d2
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-02-03 23:19:14 +0100

    Test #2

--- src/app/commands/avatar/effects/effect-extractor/ExtractEffects.ts
+++ src/app/commands/avatar/effects/effect-extractor/ExtractEffects.ts
@@ -30,6 +30,7 @@ export class ExtractEffects extends Task {
                     path.resolve(Configuration.tmpFolder, 'effects', '_extracted'),
                 );
 
+                // TEST
 
                 this.log(effect.lib + ' downloaded !');
             }
